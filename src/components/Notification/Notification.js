36ad86fd575434b4ca4135c4acdae67f20cce84f
mikiteek <mikiteek@gmail.com>
2020-09-15 14:46:03 +0300

    Add propTypes and defaultProps

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,7 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Notification = ({message}) => (
   <h2>{message}</h2>
 );
+
+Notification.defaultProps = {
+  message: "No feedback given",
+}
+
+Notification.propTypes = {
+  message: PropTypes.string,
+}
 
 export default Notification;
