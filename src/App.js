f9f126a8ec871e545399ecd1fdc94915ab283032
mikiteek <mikiteek@gmail.com>
2020-09-15 11:47:40 +0300

    Create template project completely in App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  }
+handleIncrementFeedback = type => {
+    this.setState(prevState => {
+      return {
+        [type]: prevState[type] + 1,
+      }
+    })
+}
+
   render() {
     return (
-      <h1>Work</h1>
+      <div>
+        <h1>Please leave feedback</h1>
+        <button type="submit" onClick={() => this.handleIncrementFeedback("good")}>Good</button>
+        <button type="submit" onClick={() => this.handleIncrementFeedback("neutral")}>Neutral</button>
+        <button type="submit" onClick={() => this.handleIncrementFeedback("bad")}>Bad</button>
+        <h2>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </div>
     )
   }
 }
